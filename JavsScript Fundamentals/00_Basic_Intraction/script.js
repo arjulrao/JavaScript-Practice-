533a60582c90055b4511f71edd0b9ed06347bbbb
arjulrao <arjulrao21@gmail.com>
2024-12-15 12:59:15 +0530

    JS Basic Intraction

---
+++ JavsScript Fundamentals/00_Basic_Intraction/script.js
@@ -0,0 +1,14 @@
+//alert
+alert("Press ok");
+let message = "Hey!"
+alert(message);
+
+//prompt
+let name = "UserName";
+prompt("What is your name.");
+prompt("What is you age", [22]);
+prompt(`Type ${name}.`, 'Hello');
+
+//confirm
+confirm("Are you user");
+let question = confirm("are you 18++");
---
+++ JavsScript Fundamentals/00_Basic_Intraction/task.js
@@ -0,0 +1,3 @@
+let isBoss = confirm("Are you the Boss");
+let admin = prompt(`What is the User Name`, 'Type..');
+alert(`Hello! ${admin}`);
